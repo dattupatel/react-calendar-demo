30feca4cf7c4886ff6b22dfbf527ae8e5d7a0e9c
Dattu Patel <dattu.patel@gmail.com>
2021-03-27 19:03:55 -0400

    Adding the debug code to repo

--- src/components/calendar/EventDetail.jsx
+++ src/components/calendar/EventDetail.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -30,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
 
 const EventDetail = (props) => {
 	const classes = useStyles();
+	const [ editMode, setEditMode ] = useState(true);
 	return (
 		<Card className={classes.popoverPaper}>
 			<CardContent className={classes.popoverTime}>
@@ -39,6 +40,29 @@ const EventDetail = (props) => {
 					</strong>
 				</Typography>
 			</CardContent>
+			{editMode && (
+				<CardContent>
+					<pre>{JSON.stringify(props.event.layout, null, 2)}</pre>
+					<Grid container>
+						{[ 'totalColumns', 'column', 'colspan' ].map((c) => {
+							return (
+								<Fragment key={c}>
+									<Grid item xs={6}>
+										{c}
+									</Grid>
+									<Grid item xs={6}>
+										<input
+											type="text"
+											value={props.event.layout[c]}
+											onChange={props.onChange.bind(this, c)}
+										/>
+									</Grid>
+								</Fragment>
+							);
+						})}
+					</Grid>
+				</CardContent>
+			)}
 			<CardContent>
 				<Typography variant="h5" component="h1">
 					<strong>{props.event.name}</strong>
@@ -81,9 +105,27 @@ const EventDetail = (props) => {
 				</Fragment>
 			)}
 			<CardActions className={classes.cardActions}>
-				<Button size="small" color="primary" onClick={props.handleClose}>
-					Close
-				</Button>
+				<Grid
+					container
+					direction="row"
+					justify="space-between"
+					alignItems="center"
+				>
+					<Grid item>
+						<Button
+							size="small"
+							color="primary"
+							onClick={() => setEditMode((prev) => !prev)}
+						>
+							Edit Layout
+						</Button>
+					</Grid>
+					<Grid item>
+						<Button size="small" color="primary" onClick={props.handleClose}>
+							Close
+						</Button>
+					</Grid>
+				</Grid>
 			</CardActions>
 		</Card>
 	);
